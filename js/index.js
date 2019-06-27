8356947ce9a8821e3701cffd3a8713b613448b7d
hieutst <hieunvgch16497@fpt.edu.vn>
2019-06-27 15:23:33 +0700

    finish

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	search();
 	drop_map();
-
+	drop_map_two();
 }, false)
 
 function search(){
@@ -21,7 +21,24 @@ function drop_map() {
 	var drop_map = document.querySelector('#drop-map');
 	var nen = document.querySelector('#nen');
 	var esc = document.querySelector('#esc-map');
-	console.dir(view_road);
+
+	view_road.onclick = function(){
+		drop_map.classList.toggle('show');
+		nen.classList.add('nen-den');
+	}
+
+	esc.onclick = function(){
+		drop_map.classList.remove('show');
+		nen.classList.remove('nen-den');
+	}
+}
+
+function drop_map_two() {
+	// body...
+	var view_road = document.querySelector('#view-map-2');
+	var drop_map = document.querySelector('#drop-map-2');
+	var nen = document.querySelector('#nen');
+	var esc = document.querySelector('#esc-map-2');
 
 	view_road.onclick = function(){
 		drop_map.classList.toggle('show');
